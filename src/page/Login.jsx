262f06f0802cf4f411be811497329b1600ad532f
yo-dalo <kumaradarsh00572@gmail.com>
2025-12-15 21:53:48 +0530

    setup

---
+++ src/page/Login.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='w-screen px-30 py-24 gap-14 flex justify-center items-center h-screen bg-white'>
+
+
+        <div className='w-1/3  bg-blue-900 h-full'>
+          <h1><img src="" alt="" /></h1>
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+  <div className='w-1/2  rounded-2xl bg-[#FFF1F0] flex flex-col justify-center items-center  h-full'>
+
+
+
+<div className='w-full px-7 '>
+  <img src="../../public/img/login.png"  alt="" />
+</div>
+
+
+<div className='text-center'>
+ <h3 className='font-bold py-3 text-lx'>Connect. Grow. Succeed.</h3>
+ <h4 className='text-xs w-70'>Your gateway to the CTRM/ETRM professional community.</h4>
+</div>
+
+
+
+
+
+
+
+
+
+
+          
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Login
